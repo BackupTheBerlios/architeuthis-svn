6caa56989757e7258aa03174469e92a7c69a2321
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2005-02-11 15:16:24 +0000

    Das Verzeichnis von CLASS_FILE_PATH geändert in classes und standardmäßig auskommentiert.

--- branches/commandline/runScripts/setup.bat
+++ branches/commandline/runScripts/setup.bat
@@ -10,28 +10,23 @@ REM JAVA_HOME=/usr/java
 REM Der Pfad zur JVM (default java aus dem %PATH%)
 set JAVA=java
 
-
-REM Das Verzeichnis, in das Architeuthis installiert wurde
-set INSTALLDIR=.
-
 REM Das Verzeichnis mit den policy-, properties- und config-Dateien
 set CONFIG_DIR=..\config
 
 REM Das Verzeichnis mit den jar-Dateien für die einzelnen Komponenten
 set DEPLOY_DIR=..\deploy
 
+REM Der Pfad, unter dem die Anwendungs-Klassen bzw. deren äußerstes
+REM Package liegen. Wird für den Classfileserver verwendet und normalerweise
+REM als äußere Umgebungsvariable gesetzt. Man kann sie nachfolgend aber auch
+REM hier setzen.
+REM set CLASS_FILE_PATH=..\classes
 
 REM Der Rechner, auf dem der Dispatcher von Architeuthis läuft
 set DISPATCHER_HOST=127.0.0.1
 
 REM Der Port, unter dem der Dispatcher von Architeuthis erreichbar ist
 set DISPATCHER_PORT=1854
-
-
-
-REM Der Pfad, unter dem die Anwendungs-Klassen bzw. deren 
-REM äußerstes Package liegen, wird für den Classfileserver verwendet.
-set CLASS_FILE_PATH=..\build
 
 REM Der Port, unter dem der ClassFileServer läuft
 set CLASS_SERVER_PORT=1855
@@ -42,8 +37,6 @@ set CLASS_SERVER_HOST=127.0.0.1
 REM URL, unter dem die Problemklassen abrufbar sind
 set CLASSURL=http://%CLASS_SERVER_HOST%:%CLASS_SERVER_PORT%/
 
-
 REM Die Ausgabedatei der Lösung
 set SOLUTIONFILE=loesung
-
 
--- branches/commandline/runScripts/setup.sh
+++ branches/commandline/runScripts/setup.sh
@@ -8,15 +8,17 @@
 # Der Pfad zur JVM
 JAVA="$JAVA_HOME/bin/java"
 
-# Das Verzeichnis, in das Architeuthis installiert wurde
-INSTALLDIR="."
-
 # Das Verzeichnis mit den policy-, properties- und config-Dateien
 CONFIG_DIR="../config"
 
 # Das Verzeichnis mit den jar-Dateien für die einzelnen Komponenten
 DEPLOY_DIR="../deploy"
 
+# Der Pfad, unter dem die Anwendungs-Klassen bzw. deren äußerstes
+# Package liegen. Wird für den Classfileserver verwendet und normalerweise
+# als äußere Umgebungsvariable gesetzt. Man kann sie nachfolgend aber auch
+# hier setzen.
+# CLASS_FILE_PATH="../classes"
 
 # Der Rechner, auf dem der Dispatcher von Architeuthis läuft
 #DISPATCHER_HOST=isny.informatik.uni-stuttgart.de
@@ -24,11 +26,6 @@ DISPATCHER_HOST="127.0.0.1"
 
 # Der Port, unter dem der Dispatcher von Architeuthis erreichbar ist
 DISPATCHER_PORT="1854"
-
-
-# Der Pfad, unter dem die Anwendungs-Klassen bzw. deren äußerstes
-# Package liegen, wird für den Classfileserver verwendet.
-CLASS_FILE_PATH="../build"
 
 # Der Port, unter dem der ClassFielServer läuft
 CLASS_SERVER_PORT="1855"
@@ -41,8 +38,6 @@ CLASS_SERVER_HOST="127.0.0.1"
 #CLASSURL=http://www.iis.uni-stuttgart.de/forschung/RMI-Test/
 CLASSURL="http://${CLASS_SERVER_HOST}:${CLASS_SERVER_PORT}/"
 
-
 # Die Ausgabedatei der Lösung
 SOLUTIONFILE="loesung"
-
 
