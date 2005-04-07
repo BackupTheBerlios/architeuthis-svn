27c55a67175cd63781e9506d2a5ec0aefbbd4ff0
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2005-04-07 14:24:30 +0000

    Textformat nach Unix konvertiert und Aufruf-Parameter geändert.

--- branches/remotestore/runScripts/runRemoteHash.sh
+++ branches/remotestore/runScripts/runRemoteHash.sh
@@ -5,7 +5,7 @@
 
 
 # Benötigte Umgebungsvariablen (CONFIG_DIR, CLASSURL, DISPATCHER_HOST,
-# DISPATCHER_PORT, JAVA) werden gesetzt
+# DISPATCHER_PORT, JAVA, DEPLOY_DIR, CLASS_FILE_PATH) werden gesetzt
 . ./setup.sh
 
 
@@ -22,5 +22,5 @@ ARGS="$ARGS $DISPATCHER_HOST:$DISPATCHER_PORT"
 # die Main-Klasse
 MAIN="de.unistuttgart.architeuthis.testenvironment.hashstore.HashStoreMain"
 
-exec $JAVA  -cp ../build $JVMPAR $MAIN $ARGS
+exec $JAVA -cp $DEPLOY_DIR/User.jar:$CLASS_FILE_PATH/Problems.jar $JVMPAR $MAIN $ARGS
 
