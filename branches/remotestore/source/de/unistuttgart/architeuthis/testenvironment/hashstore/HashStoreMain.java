664197f3559d580a0b39c5478f05e3b43115cde4
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2005-04-12 15:07:44 +0000

    Zusätzliches Komandozeilenargument aufgenommen, mit dem festgelegt werden kann, daß verteilte RemoteStores erzeugt werden und wie diese Methode vom zentralen RemoteStore aufrufen.

--- branches/remotestore/source/de/unistuttgart/architeuthis/testenvironment/hashstore/HashStoreMain.java
+++ branches/remotestore/source/de/unistuttgart/architeuthis/testenvironment/hashstore/HashStoreMain.java
@@ -1,7 +1,7 @@
 /*
  * file:        HashStoreMain.java
  * created:     15.02.2005 von Michael Wohlfart
- * last change: 10.04.2005 von Dietmar Lippold
+ * last change: 12.04.2005 von Dietmar Lippold
  * developers:  Michael Wohlfart, michael.wohlfart@zsw-bw.de
  *
  * This software was developed at the Institute for Intelligent Systems at the
@@ -47,47 +47,75 @@ import de.unistuttgart.architeuthis.userinterfaces.ProblemComputeException;
 public final class HashStoreMain {
 
     /**
+     * Die Anzahl der zu erzeugenden Put-Teilprobleme.
+     */
+    private static int putCreateNr = 1;
+
+    /**
+     * Gibt an, ob nur ein zentraler RemoteStore erzeugt werden soll.
+     */
+    private static boolean centralOnly = true;
+
+    /**
+     * Gibt an, ob die dezentralen RemoteStores Methoden des zentralen
+     * RemoteStore synchron aufrufen sollen.
+     */
+    private static boolean syncron;
+
+    /**
      * Konstruktur, der nicht aufgerufen werden soll.
      */
     private HashStoreMain() {
     }
 
     /**
-     * Die Anzahl der zu erzeugenden Put-Teilprobleme.
-     */
-    private static int putCreateNr = 1;
-
-    /**
      * Die ausführbare Methode. Übergibt Architeuthis ein Problem und gibt
      * nach Erhalt die Lösung aus. Wenn nicht angegeben wird, wie viele
-     * Teilprobleme erzeugt werden sollen, wird eines erzeugt.
+     * Teilprobleme erzeugt werden sollen, wird eines erzeugt. Wenn als
+     * vierter Parameter <KBD>true</KBD> oder <KBD>false</KBD> angegeben
+     * wird, wird ruften die dezentralen RemoteStores die Methoden des
+     * zentralen synchron bzw. asynchron auf. Wenn weder das eine noch das
+     * andere angegeben wird, gibt es nur einen zentralen RemoteStore.
      *
      * @param args  Die Codebase und die Angabe des Dispatchers sowie optional
      *              die Anzahl der zu erzeugenden Put-Teilprobleme.
      */
     public static void main(String[] args) {
+        ProblemComputation computation;
+        HashStoreProblemImpl problem;
+        RemoteHashMapGenerator generator;
         Serializable solution = null;
 
-        if ((args.length < 2) || (args.length > 3)) {
+        if ((args.length < 2) || (args.length > 4)) {
             System.out.println("Bitte Codebase-URL und Dispather sowie"
                                + " optional die Anzahl der Put-Teilprobleme"
-                               + " als Argumente angeben");
+                               + " und die Art der Aufrufe als Argumente"
+                               + " angeben");
         } else {
             String codebase = args[0];
             String dispatcher = args[1];
-            if (args.length == 3) {
+            if (args.length >= 3) {
                 putCreateNr = Integer.parseInt(args[2]);
+            }
+            if ((args.length == 4)
+                    && (args[3].equals("true") || args[3].equals("false"))) {
+                centralOnly = false;
+                syncron = args[3].equals("true");
             }
 
             try {
                 // Archi User-Interface.
-                ProblemComputation computation = new ProblemComputation();
+                computation = new ProblemComputation();
 
                 // Ein Problem.
-                HashStoreProblemImpl problem = new HashStoreProblemImpl(putCreateNr);
+                problem = new HashStoreProblemImpl(putCreateNr);
 
                 // Ein RemoteStore-Generator.
-                RemoteHashMapGenerator generator = new RemoteHashMapGenerator();
+                if (centralOnly) {
+                    generator = new RemoteHashMapGenerator();
+                } else {
+                    generator = new RemoteHashMapGenerator(syncron);
+                }
 
                 System.out.println("Problem wird abgeschickt");
 
