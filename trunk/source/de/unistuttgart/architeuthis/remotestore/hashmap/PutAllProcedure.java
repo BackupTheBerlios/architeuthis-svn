61a1a4e96d4fc8ba815e50f0e56431cef1440670
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2005-04-19 11:18:12 +0000

    Merged Änderungen von branches/remotestore r17:76 in den trunk, zweites Commit.

---
+++ trunk/source/de/unistuttgart/architeuthis/remotestore/hashmap/PutAllProcedure.java
@@ -0,0 +1,66 @@
+/*
+ * file:        PutAllProcedure.java
+ * created:     17.04.2005
+ * last change: 17.04.2005 by Dietmar Lippold
+ * developers:  Michael Wohlfart, michael.wohlfart@zsw-bw.de
+ *              Dietmar Lippold,  dietmar.lippold@informatik.uni-stuttgart.de
+ *
+ * This software was developed at the Institute for Intelligent Systems at the
+ * University of Stuttgart (http://www.iis.uni-stuttgart.de/) under leadership
+ * of Dietmar Lippold (dietmar.lippold@informatik.uni-stuttgart.de).
+ *
+ *
+ * This file is part of Architeuthis.
+ *
+ * Architeuthis is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Architeuthis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Architeuthis; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package de.unistuttgart.architeuthis.remotestore.hashmap;
+
+import java.util.Map;
+import java.rmi.RemoteException;
+
+import de.unistuttgart.architeuthis.remotestore.TransmitProcedure;
+import de.unistuttgart.architeuthis.userinterfaces.develop.RemoteStore;
+
+/**
+ * Implementiert eine Methode, die beim RelayStore für eine Map die Methode
+ * <CODE>putAll</CODE> aufruft.
+ *
+ * @author Dietmar Lippold
+ */
+public class PutAllProcedure implements TransmitProcedure {
+
+    /**
+     * Übertragt die übergebenen Map, die an den <CODE>Transmitter</CODE>
+     * übergeben wurde, zur zentralen <CODE>RelayHashMap</CODE>, indem es
+     * dort die Methode <CODE>putAll</CODE> aufruft.
+     *
+     * @param mapObject   Das zu übertragende Map.
+     * @param relayStore  Der RelayStore, zu dem das Objekt übertragen werden
+     *                    soll. Dabei handelt es sich um eine
+     *                    <CODE>RelayHashMap</CODE>.
+     *
+     * @throws RemoteException  Bei einem RMI Problem.
+     */
+    public void transmit(Object mapObject, RemoteStore relayStore)
+        throws RemoteException {
+
+        Map map = (Map) mapObject;
+        ((RelayHashMap) relayStore).putAll(map);
+    }
+}
+
---
+++ trunk/source/de/unistuttgart/architeuthis/remotestore/hashset/RelayHashSet.java
@@ -0,0 +1,79 @@
+/*
+ * file:        RelayHashSet.java
+ * created:     08.02.2005
+ * last change: 18.04.2005 by Dietmar Lippold
+ * developers:  Michael Wohlfart, michael.wohlfart@zsw-bw.de
+ *              Dietmar Lippold,  dietmar.lippold@informatik.uni-stuttgart.de
+ *
+ * This software was developed at the Institute for Intelligent Systems at the
+ * University of Stuttgart (http://www.iis.uni-stuttgart.de/) under leadership
+ * of Dietmar Lippold (dietmar.lippold@informatik.uni-stuttgart.de).
+ *
+ *
+ * This file is part of Architeuthis.
+ *
+ * Architeuthis is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Architeuthis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Architeuthis; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package de.unistuttgart.architeuthis.remotestore.hashset;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.rmi.RemoteException;
+
+import de.unistuttgart.architeuthis.userinterfaces.develop.RemoteStore;
+
+/**
+ * Dieses Interface gibt die Methoden vor, die für einen RemoteStore mit
+ * Relay-Funktion bei Verwaltung eines <CODE>HashSet</CODE> zu implementieren
+ * sind.
+ *
+ * @author Michael Wohlfart, Dietmar Lippold
+ */
+public interface RelayHashSet extends RemoteStore {
+
+    /**
+     * Speichert das übergebene Objekt und gibt es an alle RemoteStores
+     * weiter.
+     *
+     * @param object  Das zu speichernde Objekt.
+     *
+     * @throws RemoteException  Bei einem RMI Problem.
+     */
+    public void add(Serializable object) throws RemoteException;
+
+    /**
+     * Speichert die Objekte der übergebenen <CODE>Collection</CODE>, die
+     * serialisierbar sein müssen, und gibt die <CODE>Collection</CODE> an
+     * alle RemoteStores weiter.
+     *
+     * @param collection  Die Collection der zu speichernden Objekte.
+     *
+     * @throws RemoteException  Bei einem RMI Problem.
+     */
+    public void addAll(Collection collection) throws RemoteException;
+
+    /**
+     * Liefert eine Kopie des gespeicherten <CODE>HashSet</CODE>.
+     *
+     * @return  Den Speicherinhalt.
+     *
+     * @throws RemoteException  Bei einem RMI Problem.
+     */
+    public HashSet getHashSet() throws RemoteException;
+}
+
