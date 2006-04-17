f8cc9491c0615b596cca693d379483e69c894469
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2006-04-17 09:37:57 +0000

    Eine Variable für die Optionen der Anwendung definiert und einen Kommentar zur Angabe weiterer Optionen auf der Kommandozeile hinzugefügt.

--- trunk/runScripts/runDispatcher.bat
+++ trunk/runScripts/runDispatcher.bat
@@ -19,9 +19,14 @@ set JVMPAR=%JVMPAR% -Djava.security.policy=%CONFIG_DIR%/%POLICY_CONF%
 set JVMPAR=%JVMPAR% -Djava.rmi.server.RMIClassLoaderSpi=%CLASSLOADER_SPI%
 set JVMPAR=%JVMPAR% -Djava.util.logging.config.file=%CONFIG_DIR%/%LOGGING_CONF%
 
+REM die Parameter für die Anwendung
+set ARGS=
+set ARGS=%ARGS% -port %DISPATCHER_PORT%
+
+REM Auf der Kommandozeile können die Optionen -d und -t angegeben werden.
 
 REM die Main-Klasse ist
 REM   de.unistuttgart.architeuthis.dispatcher.DispatcherImpl
 REM und wird als Main-Class Attribut im jar-Manifest definiert
 
-%JAVA% %JVMPAR% -jar %DEPLOY_DIR%\Dispatcher.jar -port %DISPATCHER_PORT%
+%JAVA% %JVMPAR% -jar %DEPLOY_DIR%\Dispatcher.jar %ARGS% %1 %2
--- trunk/runScripts/runDispatcher.sh
+++ trunk/runScripts/runDispatcher.sh
@@ -18,9 +18,15 @@ JVMPAR="$JVMPAR -Djava.security.policy=$CONFIG_DIR/$POLICY_CONF"
 JVMPAR="$JVMPAR -Djava.rmi.server.RMIClassLoaderSpi=$CLASSLOADER_SPI"
 JVMPAR="$JVMPAR -Djava.util.logging.config.file=$CONFIG_DIR/$LOGGING_CONF"
 
+# die Parameter für die Anwendung
+ARGS=" "
+ARGS="$ARGS -port $DISPATCHER_PORT"
+
+# Auf der Kommandozeile können die Optionen -d und -t oder angegeben werden.
+
 # die Main-Klasse ist
 #   de.unistuttgart.architeuthis.dispatcher.DispatcherImpl
 # und wird als Main-Class Attribut im jar-Manifest definiert
 
-exec $JAVA $JVMPAR -jar $DEPLOY_DIR/Dispatcher.jar -port $DISPATCHER_PORT
+exec $JAVA $JVMPAR -jar $DEPLOY_DIR/Dispatcher.jar $ARGS $@
 
