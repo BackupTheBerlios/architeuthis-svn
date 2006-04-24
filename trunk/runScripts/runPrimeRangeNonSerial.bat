7f5ba0e6befe38876d4e6f2410a7bcd146e0b1cc
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2006-04-24 20:10:34 +0000

    Scripte kopiert, um die im Verzeichnis prime anschließend zu ändern.

---
+++ trunk/runScripts/runPrimeRangeNonSerial.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Startet den Problem-Transmitter und übermittelt dem Dispatcher das
+REM Primzahlbereich-Problem
+
+
+REM Benötigte Umgebungsvariablen (CONFIG_DIR, CLASSURL, DISPATCHER_HOST,
+REM DISPATCHER_PORT, SOLUTIONFILE, JAVA, DEPLOY_DIR) werden gesetzt
+call setup.bat
+
+
+REM die Parameter für die JVM
+set JVMPAR=
+set JVMPAR=%JVMPAR% -Djava.security.policy=%CONFIG_DIR%/transmitter.pol
+
+REM die Parameter für die Anwendung
+set ARGS=
+set ARGS=%ARGS% -u %CLASSURL%
+set ARGS=%ARGS% -r %DISPATCHER_HOST%:%DISPATCHER_PORT%
+set ARGS=%ARGS% -c de.unistuttgart.architeuthis.testenvironment.prime.advanced.PrimeRangeProblemImpl
+REM set ARGS=%ARGS% -f %SOLUTIONFILE%
+
+# Auf der Kommandozeile können die Optionen -d und entweder -p oder -n
+# angegeben werden.
+
+REM die Main-Klasse
+set MAIN=de.unistuttgart.architeuthis.user.ProblemTransmitterApp
+
+%JAVA% -cp %DEPLOY_DIR%/User.jar %JVMPAR% %MAIN% %ARGS% %1 %2
---
+++ trunk/runScripts/runPrimeSequenceSerial.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Startet den Problem-Transmitter und übermittelt dem Dispatcher das
+# serialisierte Primzahlfolge-Problem
+
+
+# Benötigte Umgebungsvariablen (CONFIG_DIR, CLASSURL, DISPATCHER_HOST,
+# DISPATCHER_PORT, SOLUTIONFILE, JAVA, DEPLOY_DIR, CLASS_FILE_PATH) werden
+# gesetzt
+. ./setup.sh
+
+# die Parameter für die JVM
+JVMPAR=" "
+JVMPAR="$JVMPAR -Djava.security.policy=$CONFIG_DIR/transmitter.pol"
+
+# die Parameter für die Anwendung
+ARGS=" "
+ARGS="$ARGS -s"
+ARGS="$ARGS -u $CLASSURL"
+ARGS="$ARGS -r $DISPATCHER_HOST:$DISPATCHER_PORT"
+ARGS="$ARGS -c de.unistuttgart.architeuthis.testenvironment.prime.advanced.PrimeSequenceProblemImpl"
+# ARGS="$ARGS -f $SOLUTIONFILE"
+
+# Auf der Kommandozeile können die Optionen -d und entweder -p oder -n
+# angegeben werden.
+
+# die Main-Klasse
+MAIN="de.unistuttgart.architeuthis.user.ProblemTransmitterApp"
+
+exec $JAVA -cp $DEPLOY_DIR/User.jar:$CLASS_FILE_PATH $JVMPAR $MAIN $ARGS $@
+
