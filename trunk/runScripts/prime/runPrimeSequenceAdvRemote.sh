7381710ed7ba9b3ce8829d88635ab4993aa8d5a6
lippold <lippold@a955f504-240d-1043-abc6-739c3196342c>
2006-04-25 08:48:47 +0000

    Scripte erzeugt, die die advanced-Klassen und die Klasse ProblemComputation benutzen.

---
+++ trunk/runScripts/prime/runPrimeSequenceAdvRemote.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Berechnet die Primzahlen unter Verwendung der Klasse ProblemComputation
+# auf dem Compute-System
+
+
+# Benötigte Umgebungsvariablen (JAVA, DEPLOY_DIR, CONFIG_DIR) werden gesetzt
+. ./setup.sh
+
+# die Grenzen für das Zahlen-Intervall, aus dem die Primzahlen ermittelt
+# werden sollen
+LOWER_BORDER="200000"
+UPPER_BORDER="201000"
+
+# die Parameter für die JVM
+JVMPAR=" "
+JVMPAR="$JVMPAR -Djava.security.policy=$CONFIG_DIR/transmitter.pol"
+
+# die Parameter für die Anwendung
+ARGS=" "
+ARGS="$ARGS $LOWER_BORDER $UPPER_BORDER"
+ARGS="$ARGS n"
+
+# die Main-Klasse
+MAIN="de.unistuttgart.architeuthis.testenvironment.prime.advanced.GeneratePrimes"
+
+exec $JAVA -cp $DEPLOY_DIR/User.jar:$DEPLOY_DIR/Problems.jar $JVMPAR $MAIN $ARGS
+
